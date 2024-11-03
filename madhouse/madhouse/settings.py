3df8a5a5a306b878eccb4e07a8712d8c97c0923b
Alexander <alexpunder@yandex.ru>
2024-11-03 15:51:32 +0300

    Добавлены файлы инфраструктуры. Подключены postgres, redis, nginx. Добавлены Dockerfile, docker compose для сборки.

--- madhouse/madhouse/settings.py
+++ madhouse/madhouse/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'madhouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '1234'),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
